42ecaccb1b7bc911de71079710e33403b50695c4
Tim Lai <tuanvu0995@gmail.com>
2023-11-11 10:56:21 +0700

    fix: Use experimental-edge
    Signed-off-by: Tim Lai <tuanvu0995@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/main.scss";
 
-export const runtime = "edge";
+export const runtime = "experimental-edge";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
